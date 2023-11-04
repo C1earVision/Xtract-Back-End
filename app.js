71056ca2f7761ce5242b5248352666b7ba518a3e
C1earVision <ahmedebrahim3690@gmail.com>
2023-11-04 12:36:06 +0200

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('trust proxy', 1);
 app.use(express.json());
 app.use(helmet())
 app.use(xss())
-app.use(cors(['http://127.0.0.1:5500']))
+app.use(cors())
 app.use(rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, //limit each ip to 100 requests per windowMs
